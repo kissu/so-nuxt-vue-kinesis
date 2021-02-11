1da7a7cd79952720491ba8d3b339a851a0e4661c
Konstantin BIFERT <konstantin.bifert@gmail.com>
2021-02-11 11:19:20 +0100

    done

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="main">
+    <kinesis-container>
+      <kinesis-element type="rotate" :strength="20">
+        <v-img
+          class="mb-5 image"
+          src="https://source.unsplash.com/500x350/?japan"
+          alt="Cell"
+        />
+      </kinesis-element>
+    </kinesis-container>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
